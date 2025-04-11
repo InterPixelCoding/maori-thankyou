046639e66bc507deeb8f6171125496baa2aba33f
InterPixel <theweeklyjubs@gmail.com>
2025-04-11 19:36:41 +0100

    Build 0.0.2

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const colour_inputs = document.querySelectorAll(`input[type="color"]`);
 colour_inputs.forEach(function(input, index) {
     input.addEventListener("input", (e) => {
-        console.log(e.target.value)
         document.querySelector(':root').style.setProperty(`--${input.id}`, e.target.value)
     })
 })
@@ -25,4 +24,31 @@ function typewriter(text_el, speed, delay = 0) {
     }, delay);
 }
 
-typewriter(document.querySelector("h1"), 100, 10000)
+typewriter(document.querySelector("h1"), 100, 10000);
+
+async function fetch_json(sheet_url, asdf = "AIzaSyAM07AIfBXXRU0Y8MbpzySSVtCAG3xjHr0") {
+    try {
+        const response = await fetch(`${sheet_url}?key=${asdf}`);
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        return await response.json();
+    } catch (error) {
+        console.error('Error fetching JSON:', error);
+        throw error; // Re-throw the error to allow handling by caller
+    }
+}
+
+fetch_json('https://sheets.googleapis.com/v4/spreadsheets/1zjRNYIoJHSVrsQmtPnAIGiT7ER851TkQE9bgxqoL86Q/values/Birthday_Cards')
+.then(response => {
+    const code = window.location.href.split("?=")[1];
+    response.values.forEach(arr => {
+        if(arr.includes(code)) {
+            document.querySelector("h2").textContent = `Dear ${arr[0]},`;
+            document.querySelector("p").textContent = `${arr[1]}`;
+        }
+    })
+})
+.catch(error => {
+    console.error('Fetch failed:', error);
+});
